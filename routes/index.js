79292486b8f1df5be2cf0062a06fc693fe1233dd
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-14 10:13:56 -0500

    passing variables to the pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Home");
+  res.render("inicio");
 });
 
 router.get("/nosotros", (req, res) => {
