aecc12beeb67a5dc1b3f534762aa113cb5cb789c
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-13 21:35:54 -0500

    enabling imports and exports in node.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
 // Definir puerto
 const port = process.env.PORT || 4000;
+
+app.get("/", (request, response) => {
+  // request - lo que enviamos : response - lo que express nos responde //
+  response.send("inicio");
+});
+app.get("/nosotros", (req, res) => {
+  res.send("Nosotros");
+});
+app.get("/contacto", (req, res) => {
+  res.send("Contacto");
+});
 
 app.listen(port, () => {
   console.log(`El servidor estáa funcionando en el puerto ${port}`);
