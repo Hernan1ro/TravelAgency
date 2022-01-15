2f8d0762f3db6483489575fdd2c77f76972b9b75
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-15 10:33:44 -0500

    Connecting to the mysql DB

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import router from "./routes/index.js";
+import db from "./config/db.js";
 
 const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+  .then(() => console.log("Base de datos conectada"))
+  .catch((err) => console.log(err));
 
 // Definir puerto
 const port = process.env.PORT || 4000;
