b753f5560bcde412c6855bef900b086286d7da8c
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-16 16:43:50 -0500

    deployment configuration

--- config/db.js
+++ config/db.js
@@ -1,19 +1,26 @@
 import Sequelize from "sequelize";
+import dotenv from "dotenv";
+dotenv.config({ path: "variables.env" });
 
-const db = new Sequelize("agenciaviajes", "root", "", {
-  host: "127.0.0.1",
-  port: "3306",
-  dialect: "mysql",
-  define: {
-    timestamps: false,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  operatorAliases: false,
-});
+const db = new Sequelize(
+  process.env.BD_NOMBRE,
+  process.env.BD_USER,
+  process.env.BD_PASS,
+  {
+    host: process.env.BD_HOST,
+    port: process.env.BD_PORT,
+    dialect: "mysql",
+    define: {
+      timestamps: false,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    operatorAliases: false,
+  }
+);
 
 export default db;
